e567751fdcf5135486d4937b75c3d46d7308110b
monkukui <monkukui1953@gmail.com>
2020-06-16 16:14:56 +0900

    Add: Hello Tab コンポーネントを追加

--- problem-queue-frontend/src/App.tsx
+++ problem-queue-frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DisplayHelloTab from './components/DisplayHelloTab';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <DisplayHelloTab />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ problem-queue-frontend/src/components/DisplayHelloTab.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function DisplayHelloTab() {
+  return (
+    <span>Hello Tab</span>
+  );
+}
+
+export default DisplayHelloTab;
