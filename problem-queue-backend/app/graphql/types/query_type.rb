1854b39bdef358394368cf78bdaf839c2cb93ebb
monkukui <monkukui1953@gmail.com>
2020-08-08 19:36:43 +0900

    Delete test query saying hello

--- problem-queue-backend/app/graphql/types/query_type.rb
+++ problem-queue-backend/app/graphql/types/query_type.rb
@@ -2,14 +2,6 @@ module Types
   class QueryType < Types::BaseObject
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
-
     field :user, Types::UserType, null: false do
       description 'ユーザ情報を1件取得する'
       argument :id, Int, required: true, description: 'ユーザID'
